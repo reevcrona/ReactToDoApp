857a7caf8b9a05d5231e66f7c54c5b3a81e45569
Yashendra25 <yashendrauraj25@gmail.com>
2024-07-06 23:29:41 +0530

    configues

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  // Default port if VITE_PORT is not defined
-  const port = parseInt(import.meta.env.VITE_PORT) || 5173;
-  return {
-    plugins: [react()],
-    server: {
-      host: '0.0.0.0',
-      port: port,
-    },
-  };
-});
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
