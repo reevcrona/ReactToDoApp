24c8dd28cc1404acaaea69388f6313f583cce89c
reevcrona <89569507+reevcrona@users.noreply.github.com>
2025-01-27 18:43:47 +0100

    added interface for input props and added type to event parameter

--- src/components/Todoinput.tsx
+++ src/components/Todoinput.tsx
@@ -1,20 +1,25 @@
 import { useState } from 'react';
 import './Todoinput.css';
 import MaterialIcon from 'material-icons-react';
-export default function TodoInput({onAddItem}){
-    var [currentInputValue,setcurrentInputValue] = useState('');
+
+interface todoInputProps{
+    onAddItem:(todoItem:string) => void
+}
+
+export default function TodoInput({onAddItem}:todoInputProps){
+    const [currentInputValue,setcurrentInputValue] = useState('');
     function addToDoItem(){
         onAddItem(currentInputValue);
         setcurrentInputValue('');
         
     }
-    function onInputChange(event){
+    function onInputChange(event:React.ChangeEvent<HTMLInputElement>){
         setcurrentInputValue(event.target.value)
     }
     return(
         <>
         <div className="todo-input-div">
-            <input className='todo-input' type='text' placeholder='Enter your todo here' onChange={onInputChange} value={currentInputValue} />
+            <input className='todo-input' type='text' placeholder='Enter your todo here' onChange={(e) => onInputChange(e)} value={currentInputValue} />
             <button className='todo-input-add' onClick={addToDoItem}>
                 < MaterialIcon icon='add' />
             </button>
