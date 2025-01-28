7e2572191285190a5a1d36d188d753196e33dbe6
reevcrona <89569507+reevcrona@users.noreply.github.com>
2025-01-28 16:22:36 +0100

    made a renderTasks function that take a taskArray parameter so we can use this function to render todoItems and completedItems

--- src/App.tsx
+++ src/App.tsx
@@ -30,6 +30,18 @@ function App() {
     setTodoItems(todoItems.filter((item) => item.id !== todoitem.id) )
     setCompletedItems(completedItems.filter((item) => item.id !== todoitem.id) )
   }
+
+  function renderTasks(taskArray:Task[]):JSX.Element[]{
+   return taskArray.map((item) =>
+   <TodoItem 
+      key={item.id} 
+      task= {item} 
+      onDelete={deleteTodo} 
+      onComplete={completeTodoItem} 
+    /> )
+  }
+
+
   return (
     <div className="app">
       <h1 className='title'>ToDo List</h1>
@@ -37,11 +49,11 @@ function App() {
       <div  className='list-block'>
         <h2 className='title'>Todo Items</h2> 
         <hr/>
-        {todoItems.map((todo) => <TodoItem key={todo.id} task= {todo} onDelete={deleteTodo} onComplete={completeTodoItem} />)}
+        {renderTasks(todoItems)}
       </div>
       <div className='list-block'>
         <h2 className='title'>Completed Items</h2>
-        {completedItems.map((todo) => <TodoItem key={todo.id} task={todo} onDelete={deleteTodo}  onComplete={completeTodoItem}/>)}
+        {renderTasks(completedItems)}
 
         <hr/>
       </div>
