f1d0dc6998b233d0ab109811d3b1901adda307fd
reevcrona <89569507+reevcrona@users.noreply.github.com>
2025-01-31 17:54:04 +0100

    added tailwin prettier plugin

--- src/components/Todoitem.tsx
+++ src/components/Todoitem.tsx
@@ -1,27 +1,28 @@
-import './Todoitem.css'
-import MaterialIcon from 'material-icons-react';
-import { TaskProps } from '../types';
+import "./Todoitem.css";
+import MaterialIcon from "material-icons-react";
+import { TaskProps } from "../types";
 
+export default function TodoItem({ task, clickHandlers }: TaskProps) {
+  return (
+    <div className="flex items-center justify-center rounded-lg bg-red-500 p-4 text-white">
+      <div className="todo-title">{task.title}</div>
 
-
-
-export default function TodoItem({task,clickHandlers}: TaskProps){
-    
-    return(
-        <div className='todo-item'>
-            <div className="todo-title">{task.title}</div>
-
-            <div className="icons-group">
-                {!task.isCompleted && <div className="todo-complete" onClick={() => clickHandlers.onComplete(task)}>
-                    <MaterialIcon icon='task_alt' color='white'/>
-                </div>
-                }
-                <div className="todo-delete" onClick={() => clickHandlers.onDelete(task)}>
-                    <MaterialIcon icon='delete' color='white'/>
-                </div>
-            </div>
-          
+      <div className="icons-group">
+        {!task.isCompleted && (
+          <div
+            className="todo-complete"
+            onClick={() => clickHandlers.onComplete(task)}
+          >
+            <MaterialIcon icon="task_alt" color="white" />
+          </div>
+        )}
+        <div
+          className="todo-delete"
+          onClick={() => clickHandlers.onDelete(task)}
+        >
+          <MaterialIcon icon="delete" color="white" />
         </div>
-
-    )
+      </div>
+    </div>
+  );
 }
